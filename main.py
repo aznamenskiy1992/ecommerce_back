1844adfbf087e8385de376a1eec04521ac861141
artem znamenskiy <aznamenskiy1992@gmail.com>
2025-07-15 23:16:34 +0300

    заполнил README.md и добавил docstring и комментарии к коду в модуль main

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 class Category:
-    """Класс хранит названия категорий и список товаров категории"""
+    """Класс для представления категории товаров в магазине.
+
+    Атрибуты:
+        product_count (int): Общее количество товаров во всех категориях (статический)
+        category_count (int): Общее количество категорий (статический)
+        name (str): Название категории
+        description (str): Описание категории
+        products (list): Список товаров в данной категории
+
+    Методы:
+        __init__: Инициализирует категорию и обновляет счетчики
+    """
 
     product_count: int = 0
     category_count: int = 0
@@ -9,16 +20,37 @@ class Category:
     products: list
 
     def __init__(self, name, description, products):
+        """Инициализирует экземпляр класса Category.
+
+        Args:
+            name (str): Название категории
+            description (str): Описание категории
+            products (list): Список товаров в категории
+
+        Примечание:
+            При инициализации автоматически увеличивает счетчики:
+            - product_count на количество товаров в категории
+            - category_count на 1
+        """
         self.name = name
         self.description = description
         self.products = products
 
+        # Обновляем общее количество товаров
         Category.product_count += len(self.products)
+        # Увеличиваем счетчик категорий
         Category.category_count += 1
 
 
 class Product:
-    """Класс хранит информацию по товарам магазина"""
+    """Класс для представления товара в магазине.
+
+    Атрибуты:
+        name (str): Название товара
+        description (str): Описание товара
+        price (int): Цена товара
+        quantity (int): Количество товара в наличии
+    """
 
     name: str
     description: str
@@ -26,6 +58,14 @@ class Product:
     quantity: int
 
     def __init__(self, name, description, price, quantity):
+        """Инициализирует экземпляр класса Product.
+
+        Args:
+            name (str): Название товара
+            description (str): Описание товара
+            price (int): Цена товара
+            quantity (int): Количество товара в наличии
+        """
         self.name = name
         self.description = description
         self.price = price
