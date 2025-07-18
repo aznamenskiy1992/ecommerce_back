12436a0c62a695357299c8de9a346cb7f75116df
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-07-18 20:25:10 +0300

    добавил docstring и комментарии к коду в модуль product

--- src/product.py
+++ src/product.py
@@ -4,18 +4,36 @@ class Product:
     Атрибуты:
         name (str): Название товара
         description (str): Описание товара
-        price (float): Цена товара
+        price (property): Свойство для работы с ценой товара (с проверкой валидности)
         quantity (int): Количество товара в наличии
+
+    Методы:
+        new_product: Создает новый товар из словаря с данными (классовый метод)
     """
 
     name: str
     description: str
-    price: float
+    __price: float  # Приватный атрибут для хранения цены
     quantity: int
 
     @classmethod
     def new_product(cls, product: dict):
-        """Возвращает созданный товар"""
+        """Фабричный метод для создания нового товара из словаря.
+
+        Args:
+            product (dict): Словарь с данными о товаре, должен содержать ключи:
+                - name: название товара
+                - description: описание товара
+                - price: цена товара
+                - quantity: количество товара
+
+        Returns:
+            Product: Созданный экземпляр класса Product
+
+        Примечание:
+            Используется для удобного создания товаров из данных,
+            например, при загрузке из JSON или базы данных
+        """
         return cls(
             name=product["name"],
             description=product["description"],
@@ -29,23 +47,39 @@ class Product:
         Args:
             name (str): Название товара
             description (str): Описание товара
-            price (int): Цена товара
+            price (float): Цена товара (должна быть положительной)
             quantity (int): Количество товара в наличии
+
+        Примечание:
+            Цена хранится в приватном атрибуте __price для контроля валидности
         """
         self.name = name
         self.description = description
-        self.__price = price
+        self.__price = price  # Устанавливаем через приватный атрибут
         self.quantity = quantity
 
     @property
     def price(self):
-        """Возвращает цену товара"""
+        """Геттер для получения цены товара.
+
+        Returns:
+            float: Текущая цена товара
+        """
         return self.__price
 
     @price.setter
     def price(self, new_price: float):
-        """Изменяет цену товара"""
+        """Сеттер для изменения цены с проверкой валидности.
+
+        Args:
+            new_price (float): Новая цена товара
+
+        Примечание:
+            Если новая цена меньше или равна 0, изменение не происходит
+            и выводится предупреждение
+        """
         if new_price <= 0:
+            # Защита от некорректного значения цены
             print("Цена не должна быть нулевая или отрицательная")
         else:
             self.__price = new_price
